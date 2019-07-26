8d2823ac72f5942ef2dd61db9da8c07ad9b36f2a
Ben Swift <=ben.swift@zivato.com>
2019-07-26 23:49:21 +0100

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-UIDesignManager_Example/Pods-UIDesignManager_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIDesignManager_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UIDesignManager_Example
+@end
---
+++ Example/Pods/Target Support Files/UIDesignManager/UIDesignManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIDesignManager : NSObject
+@end
+@implementation PodsDummy_UIDesignManager
+@end
