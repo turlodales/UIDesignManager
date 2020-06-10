d721672a4efbef81246485937c3fcece25e1c731
Ben Swift <=ben.swift@zivato.com>
2020-06-10 17:38:46 +0100

    SwiftUI Initializers

--- Source/ZButton.swift
+++ Source/ZButton.swift
@@ -9,8 +9,8 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct ZButton: View {
-
+public struct ZButton: View {
+   
     @State var name : String
     @State var text : String
     @State var defaultBackgroundHex : String
@@ -21,12 +21,25 @@ struct ZButton: View {
     @State var fontSize : CGFloat
     @State var width : CGFloat
     @State var height: CGFloat
-    
     @State var action : (() -> Void)
+    
+    public init(name: String, text: String, defaultBackgroundHex: String, defaultForegroundHex:String, cornerRadius: CGFloat, SFIcon: String, font: String, fontSize: CGFloat, width: CGFloat, height: CGFloat, action: @escaping (() -> Void)) {
+        self._name = State(initialValue: name)
+        self._text = State(initialValue: text)
+        self._defaultBackgroundHex = State(initialValue: defaultBackgroundHex)
+        self._defaultForegroundHex = State(initialValue: defaultForegroundHex)
+        self._cornerRadius = State(initialValue: cornerRadius)
+        self._SFIcon = State(initialValue: SFIcon)
+        self._font = State(initialValue: font)
+        self._fontSize = State(initialValue: fontSize)
+        self._width = State(initialValue: width)
+        self._height = State(initialValue: height)
+        self._action = State(initialValue: action)
+    }
     
     @ObservedObject var zParams = ZObject()
     
-    var body: some View {
+    public var body: some View {
         
         Button(action: action) {
             
@@ -56,7 +69,6 @@ struct ZButton: View {
                         Text(zParams.text)
                     }
                     
-                    
                 }
                 .frame(width: zParams.width, height: zParams.height, alignment: .center)
                 .background(zParams.color)
@@ -68,12 +80,10 @@ struct ZButton: View {
                 .edgesIgnoringSafeArea(.all)
             }
         }.buttonStyle(BorderlessButtonStyle())//IMPORTANT
-        
-
-            
-        
+         
     }
     
+  
     func UpdateUI() {
         zParams.styleButton(name: name, defBackgroundHex: defaultBackgroundHex, defForegroundHex: defaultForegroundHex, text: text, cornerRadius: cornerRadius, SFIcon: SFIcon, font: font, fontSize: fontSize, fWidth: width, fHeight: height)
     }
@@ -87,7 +97,7 @@ func test() {
 struct ZButton_Previews: PreviewProvider {
     @available(iOS 13.0.0, *)
     static var previews: some View {
-        ZButton(name: "sample", text: "Hello World", defaultBackgroundHex: "#FF0000", defaultForegroundHex: "#FFFFFF", cornerRadius: 5, SFIcon: "chevron.right", font: "Avenir-Medium", fontSize: 15.0, width: 100.0, height: 50.0, action: test, zParams: ZObject())
+        ZButton(name: "sample", text: "Hello World", defaultBackgroundHex: "#FF0000", defaultForegroundHex: "#FFFFFF", cornerRadius: 5, SFIcon: "chevron.right", font: "Avenir-Medium", fontSize: 15.0, width: 100.0, height: 50.0, action: test)
     }
 }
 
--- Source/ZText.swift
+++ Source/ZText.swift
@@ -22,6 +22,19 @@ struct ZText: View {
     @State var fontSize : CGFloat
     @State var width : CGFloat
     @State var height: CGFloat
+    
+    public init(name: String, text: String, defaultBackgroundHex: String, defaultForegroundHex:String, cornerRadius: CGFloat, SFIcon: String, font: String, fontSize: CGFloat, width: CGFloat, height: CGFloat) {
+        self._name = State(initialValue: name)
+        self._text = State(initialValue: text)
+        self._defaultBackgroundHex = State(initialValue: defaultBackgroundHex)
+        self._defaultForegroundHex = State(initialValue: defaultForegroundHex)
+        self._cornerRadius = State(initialValue: cornerRadius)
+        self._SFIcon = State(initialValue: SFIcon)
+        self._font = State(initialValue: font)
+        self._fontSize = State(initialValue: fontSize)
+        self._width = State(initialValue: width)
+        self._height = State(initialValue: height)
+    }
         
     @ObservedObject var zParams = ZObject()
     
@@ -53,7 +66,7 @@ func ZTextTest() {
 struct ZText_Previews: PreviewProvider {
     @available(iOS 13.0.0, *)
     static var previews: some View {
-        ZText(name: "sample", text: "Hello World", defaultBackgroundHex: "#FF0000", defaultForegroundHex: "#FFFFFF", cornerRadius: 5, SFIcon: "chevron.right", font: "Avenir-Medium", fontSize: 15.0, width: 100.0, height: 50.0, zParams: ZObject())
+        ZText(name: "sample", text: "Hello World", defaultBackgroundHex: "#FF0000", defaultForegroundHex: "#FFFFFF", cornerRadius: 5, SFIcon: "chevron.right", font: "Avenir-Medium", fontSize: 15.0, width: 100.0, height: 50.0)
     }
 }
 
