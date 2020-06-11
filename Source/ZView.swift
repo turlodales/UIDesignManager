3141dea3d9904e23bc3ceff2fc7d079c56eee1a0
Ben Swift <=ben.swift@zivato.com>
2020-06-11 09:32:00 +0100

    SwiftUI ZView

--- Source/ZView.swift
+++ Source/ZView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 
 
+@available(iOS 13.0, *)
 struct ZView: View {
 
     @State var name : String
@@ -18,10 +19,13 @@ struct ZView: View {
     @State var width : CGFloat
     @State var height: CGFloat
     
-    
-    
-    
-    
+    public init(name: String, defaultBackgroundHex: String, cornerRadius: CGFloat, width: CGFloat, height: CGFloat) {
+        self._name = State(initialValue: name)
+        self._defaultBackgroundHex = State(initialValue: defaultBackgroundHex)
+        self._cornerRadius = State(initialValue: cornerRadius)
+        self._width = State(initialValue: width)
+        self._height = State(initialValue: height)
+    }
     
     @ObservedObject var zParams = ZObject()
     
@@ -40,22 +44,14 @@ struct ZView: View {
             .edgesIgnoringSafeArea(.all)
         }
         
-
-            
-        
-    
-    
     func UpdateUI() {
         zParams.styleView(name: name, defBackgroundHex: defaultBackgroundHex, cornerRadius: cornerRadius, fWidth: width, fHeight: height)
     }
       
 }
 
-func zviewtest() {
-  print("test")
-}
-
 struct ZView_Previews: PreviewProvider {
+    @available(iOS 13.0.0, *)
     static var previews: some View {
         ZView(name: "zview_sample", defaultBackgroundHex: "#ff0000", cornerRadius: 10, width: 80, height: 80)
     }
